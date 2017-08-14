ac64b05bd78ff36e0f5e79372765b0d15192ab60
Joseph Watts <josephcw@live.com>
2017-08-14 10:57:36 -0500

    Refactor tests

--- src/test/java/com/josephwatts/tddemail/TemplateTest.java
+++ src/test/java/com/josephwatts/tddemail/TemplateTest.java
@@ -2,31 +2,33 @@ package com.josephwatts.tddemail;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TemplateTest {
 
-	@Test
-	public void oneVariable() throws Exception {
-		Template template = new Template("Hello, ${name}");
-		template.set("name", "Reader");
-		assertEquals("Hello, Reader", template.evaluate());
+	private Template template;
+	
+	@Before
+	public void setUp() throws Exception {
+		template = new Template("${one}, ${two}, ${three}.");
+		template.set("one", "1");
+		template.set("two", "2");
+		template.set("three", "3");
 	}
 	
 	@Test
 	public void multipleVariables() throws Exception {
-		Template template = new Template("${one}, ${two}, ${three}.");
-		template.set("one", "1");
-		template.set("two", "2");
-		template.set("three", "3");
-		assertEquals("1, 2, 3.", template.evaluate());
+		assertTemplateEvaluatesTo("1, 2, 3.");
 	}
 	
 	@Test
 	public void unknownVariableAreIgnored() throws Exception {
-		Template template = new Template("Hello, ${name}");
-		template.set("name", "Reader");
 		template.set("doesnotexist", "Hi.");
-		assertEquals("Hello, Reader", template.evaluate());
+		assertTemplateEvaluatesTo("1, 2, 3.");
+	}
+	
+	private void assertTemplateEvaluatesTo(String expected) {
+		assertEquals(expected, template.evaluate());
 	}
 }
